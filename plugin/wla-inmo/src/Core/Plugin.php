7fd76c1d6f8c22c505e6c9b70ef3f5fb73faa699
Wladimick <61322487+wladimick@users.noreply.github.com>
2026-09-03 19:25:11 -0400

    feat(core): add plugin lifecycle bootstrap service

---
+++ plugin/wla-inmo/src/Core/Plugin.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace WLA\Inmo\Core;
+
+final class Plugin
+{
+	private static ?self $instance = null;
+
+	private bool $booted = false;
+
+	private function __construct()
+	{
+	}
+
+	public static function instance(): self
+	{
+		if (self::$instance === null) {
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	public function boot(): void
+	{
+		if ($this->booted) {
+			return;
+		}
+
+		$this->booted = true;
+
+		add_action('init', array($this, 'loadTextDomain'), 0);
+
+		/**
+		 * Fires after WLA Inmo Core has completed its bootstrap.
+		 *
+		 * @param Plugin $plugin Plugin instance.
+		 */
+		do_action('wla_inmo_loaded', $this);
+	}
+
+	public function loadTextDomain(): void
+	{
+		load_plugin_textdomain(
+			'wla-inmo',
+			false,
+			dirname(WLA_INMO_BASENAME) . '/languages'
+		);
+	}
+
+	public function isBooted(): bool
+	{
+		return $this->booted;
+	}
+}
