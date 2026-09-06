37d7669f3143abcd2607ad57f337d14cb7ea79d1
Wladimick <61322487+wladimick@users.noreply.github.com>
2026-09-06 13:30:45 -0300

    refactor(import): keep workspace cron in core bootstrap

--- plugin/wla-inmo/src/Admin/Bootstrap.php
+++ plugin/wla-inmo/src/Admin/Bootstrap.php
@@ -18,7 +18,6 @@ final class Bootstrap
 		add_action('admin_enqueue_scripts', array(Assets::class, 'enqueue'));
 		add_action('current_screen', array(ContextHelp::class, 'add'));
 
-		\WLA\Inmo\Import\WorkspaceJanitor::register();
 		Onboarding::register();
 		SettingsPage::register();
 		ImportExportPage::register();
