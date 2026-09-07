1f55f48ea1bcab362b7d58496f075a907ee8e084
Wladimick <61322487+wladimick@users.noreply.github.com>
2026-09-07 11:05:39 -0300

    refactor(import): share source exception behavior for CSV

--- plugin/wla-inmo/src/Import/CsvException.php
+++ plugin/wla-inmo/src/Import/CsvException.php
@@ -2,28 +2,6 @@
 
 namespace WLA\Inmo\Import;
 
-use RuntimeException;
-
-final class CsvException extends RuntimeException
+final class CsvException extends SourceException
 {
-	private string $reason;
-
-	private ?int $rowNumber;
-
-	public function __construct(string $reason, string $message, ?int $rowNumber = null)
-	{
-		parent::__construct($message);
-		$this->reason = $reason;
-		$this->rowNumber = $rowNumber;
-	}
-
-	public function reason(): string
-	{
-		return $this->reason;
-	}
-
-	public function rowNumber(): ?int
-	{
-		return $this->rowNumber;
-	}
 }
